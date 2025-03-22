fe5abbf1fb4fc8f97841b121367acd80e183a5c5
Linus Pettersson <93656671+TheLinusPettersson@users.noreply.github.com>
2025-03-22 22:34:12 +0100

    Updates

--- assets/InfoCard-00903866.js
+++ assets/InfoCard-00903866.js
@@ -1,4 +1,4 @@
-import{g as f,j as n,b as m,L as u}from"./index-36256428.js";var d={exports:{}};/*!
+import{g as f,j as n,b as m,L as u}from"./index-7d238efb.js";var d={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
--- assets/NotFound-2cf50a49.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-36256428.js";const n=()=>o.jsx("div",{children:"404 page not found"});export{n as default};
